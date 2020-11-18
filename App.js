5c8b023ca2a9772895f3752abdb117902af7a6ac
czliou <48034215+czliou@users.noreply.github.com>
2020-11-17 16:53:03 -0800

    Modified CalendarComponent

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CounterComponent from './Components/CounterComponent';
+import CalendarComponent from './Components/CalendarComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <CounterComponent/>
+      <CalendarComponent/>
     </View>
   );
 }
---
+++ Components/CalendarComponent.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+
+const CalendarComponent = () => {
+  return (
+    <View>
+      <Text>
+        calendars
+      </Text>
+      <Calendar>
+        
+      </Calendar>
+    </View>
+  );
+}
+
+export default CalendarComponent;
