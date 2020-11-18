cfc66ad245b2b992401bbac596b05d3e95da5113
czliou <48034215+czliou@users.noreply.github.com>
2020-11-17 16:45:43 -0800

    Added Calendar Component

---
+++ Components/Calender.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+
+const Calendar = () => {
+  return (
+    <Calendar>
+      
+    </Calendar>
+  );
+}
+
+export default Calendar;
